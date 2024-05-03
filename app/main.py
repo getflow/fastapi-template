3dc0d1f56d9d68ca8379880b60efab0d3b729f1b
kartaris <rh@getflow.tech>
2024-05-03 19:34:16 +0300

    adds pyctuator

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import sentry_sdk
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
+from pyctuator.pyctuator import Pyctuator
 
 from app.routers.api import api_router
-from app.settings import SENTRY_DSN, SENTRY_ENVIRONMENT
+from app.settings import SENTRY_DSN, SENTRY_ENVIRONMENT, APP_NAME, APP_VERSION
 
 sentry_sdk.init(
     dsn=SENTRY_DSN,
@@ -14,11 +15,20 @@ sentry_sdk.init(
     environment=SENTRY_ENVIRONMENT,
 ) if SENTRY_DSN else None
 
-app = FastAPI(title="FastAPI template", version="0.1.0")
+app = FastAPI(title=APP_NAME, version=APP_VERSION)
 
 app.include_router(router=api_router, prefix="")
 
 # adds prometheus metrics route
 Instrumentator().instrument(app).expose(
     app, endpoint="/metrics", include_in_schema=False
+)
+
+
+pyctuator = Pyctuator(
+    app=app,
+    app_name=APP_NAME,
+    app_url="/",
+    pyctuator_endpoint_url="/pyctuator",
+    registration_url=None,
 )
--- app/settings.py
+++ app/settings.py
@@ -2,6 +2,9 @@ import os
 
 DEBUG = int(os.getenv("DEBUG", "0"))
 
+APP_NAME = os.getenv("APP_NAME", "FastAPI template")
+APP_VERSION = os.getenv("APP_VERSION", "0.1.0")
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 SENTRY_DSN = os.getenv("SENTRY_DSN")
